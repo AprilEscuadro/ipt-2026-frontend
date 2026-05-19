c10f2b5065a8d3986b7d84a9aa64b69293779c90
April Dianne D. Escuadro <156050739+AprilEscuadro@users.noreply.github.com>
2026-05-19 20:51:09 +0800

    Refactor appInitializer to use Promise for token refresh

--- src/app/_helpers/app.initializer.ts
+++ src/app/_helpers/app.initializer.ts
@@ -3,15 +3,17 @@ import { Router } from '@angular/router';
 import { AccountService } from '@app/_services';
 
 export function appInitializer(accountService: AccountService) {
-return () => {
+    return () => new Promise<void>(resolve => {
         const isResetPassword = window.location.href.includes('reset-password');
         const isVerifyEmail = window.location.href.includes('verify-email');
 
         if (isResetPassword || isVerifyEmail) {
-            return of(null);
+            resolve();
+            return;
         }
 
-        return accountService.refreshToken()
-            .pipe(catchError(() => of(null)));
-    };
+        accountService.refreshToken()
+            .pipe(catchError(() => of(null)))
+            .subscribe(() => resolve());
+    });
 }
