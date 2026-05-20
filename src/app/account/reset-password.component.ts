2e1b00fd4a7f0758bc73f493470af730996022c1
April Dianne D. Escuadro <156050739+AprilEscuadro@users.noreply.github.com>
2026-05-20 15:09:52 +0800

    Add status property to ResetPasswordComponent

--- src/app/account/reset-password.component.ts
+++ src/app/account/reset-password.component.ts
@@ -16,6 +16,7 @@ enum TokenStatus {
 export class ResetPasswordComponent implements OnInit {
     TokenStatus = TokenStatus;
     tokenStatus = TokenStatus.Validating;
+    status = 'validating';
     token?: string;
     form!: FormGroup;
     loading = false;
